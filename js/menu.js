83c815a0c8d3fbf7cef757f90d8fb134770f86b7
Vasilii Sannikov <vsannikov@five9.com>
2023-10-29 21:25:26 -0700

    feat: 3d button

---
+++ js/menu.js
@@ -0,0 +1,45 @@
+import * as THREE from 'three';
+
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js';
+import font from 'three/examples/fonts/helvetiker_regular.typeface.json';
+
+class Button {
+  model = new THREE.Group();
+  constructor(width, height, depth, color, text) {
+
+      const geometry = new THREE.BoxGeometry(width, height, depth);
+      const material = new THREE.MeshStandardMaterial({ color: color });
+      this.mesh = new THREE.Mesh(geometry, material);
+      
+      if (text) {
+        const loader = new FontLoader();
+        const threeFont = loader.parse(font);
+          const textGeometry = new TextGeometry(text, {
+              font: threeFont,
+              size: width * 0.2,
+              height: 0.0001,
+          });
+          const textMaterial = new THREE.MeshBasicMaterial({ color: 0x333333 });
+          const textMesh = new THREE.Mesh(textGeometry, textMaterial);
+          textMesh.position.set(-width / 3, -height / 5, depth / 2);
+          this.model.add(textMesh);
+      }
+
+      this.model.add(this.mesh);
+  }
+}
+
+function placeButtonNearEdge(button, camera, offsetX, offsetY, distanceFromCamera) {
+  const aspectRatio = window.innerWidth / window.innerHeight;
+  const vFov = camera.fov * Math.PI / 180;  // Преобразование в радианы
+  const viewHeightAtDistance = 2 * Math.tan(vFov / 2) * distanceFromCamera;
+  const viewWidthAtDistance = viewHeightAtDistance * aspectRatio;
+
+  // Задаем позицию кнопки с учетом смещений
+  button.position.x = (viewWidthAtDistance / 2) - offsetX;
+  button.position.y = (viewHeightAtDistance / 2) - offsetY;
+  button.position.z = -distanceFromCamera;
+}
+
+export {Button, placeButtonNearEdge};
--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import * as THREE from 'three';
 import { Stars, clickListen } from '/js/tools.js';
 import { Mount, Water, Abyss, Cloud, Fire, Board } from '/js/board.js';
 import { Figure } from '/js/figure.js';
+import { Button, placeButtonNearEdge } from '/js/menu.js';
 
 // Scene, camera, and renderer setup
 var scene = new THREE.Scene();
@@ -30,6 +31,14 @@ animate();
 
 const stars = new Stars(10000, 1000);
 scene.add(stars.draw());
+
+const button = new Button(0.01, 0.01, 0.002, 0xAAAA00, 'XYZ');
+placeButtonNearEdge(button.model, camera, 0.01, 0.01, 0.12);
+
+const camLight = new THREE.PointLight(0xFFFFFF, 0.1, 1000);
+camera.add(camLight);
+camera.add(button.model);
+scene.add(camera);
 
 const board = new Board(8, 8);
 window.board = board;
@@ -66,6 +75,7 @@ function onWindowResize() {
 
     // Обновление размеров рендерера
     renderer.setSize(window.innerWidth, window.innerHeight);
+    placeButtonNearEdge(button.model, camera, 0.01, 0.01, 0.12);
 }
 const light = new THREE.PointLight(0xFFFFFF, 70, 1000);
 light.position.set(0, 5, 0);
